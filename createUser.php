90d97cf9fce1096f950b439c43b9c1ee5857dbfc
Jo Beattie <beattiejo1@gmail.com>
2015-11-18 15:14:39 +0000

    changed local userID

--- createUser.php
+++ createUser.php
@@ -1,7 +1,7 @@
 <?php
     //if($_POST['passwordconfirm'] == $_POST['password']){
     //$sql= "INSERT INTO User VALUES(" . "'" . $_POST['userID'] . "', '" . $_POST['password'] . "', '" . $_POST['firstName'] . "', '" . $_POST['lastName'] . "', '" . $_POST['email'] . "', 0);";
-    $sql = "INSERT INTO User VALUES('jordannbeattie', 'Welcome1', 'Jo', 'Beattie', 'beattiejo1@gmail.com', '0');";
+    $sql = "INSERT INTO User VALUES('beattiejo', 'Welcome1', 'Jo', 'Beattie', 'beattiejo1@gmail.com', '0');";
     $results=mysqli_query($conn, $sql);
     header("Location: user.php");
     //}
