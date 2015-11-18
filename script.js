4385c72ef88d0f3e6aa7f873c93bbf2bb4df0321
LiamBowie <liambowie16+GitHub@gmail.com>
2015-11-18 18:21:51 +0000

    Smooth scrolling should now work on the top navbar

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     // Add smooth scrolling to all links in navbar + footer link
-    $(".sticky-sidebar a").on('click', function(event) {
+    $(".sticky-sidebar a, .navbar a").on('click', function(event) {
 
         // Prevent default anchor click behavior
         event.preventDefault();
