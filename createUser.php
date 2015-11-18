aa5309135babd23634b33890c55e574e603749e9
Jo Beattie <beattiejo1@gmail.com>
2015-11-18 16:17:45 +0000

    removed real escape

--- createUser.php
+++ createUser.php
@@ -1,7 +1,8 @@
 <?php
     include'connect.php';
     //if($_POST['passwordconfirm'] == $_POST['password']){
-    $userID =  mysqli_real_escape_string($_POST['userID']);
+    $userID=$_POST['userID'];
+    //$userID =  mysqli_real_escape_string($_POST['userID']);
     echo $userID;
     //$sql= "INSERT INTO User VALUES(" . "'" . $userID . "', '" . $_POST['password'] . "', '" . $_POST['firstName'] . "', '" . $_POST['lastName'] . "', '" . $_POST['email'] . "', 0);";
     //$sql = "INSERT INTO User VALUES('jordannorman', 'Welcome1', 'Jo', 'Beattie', 'beattiejo1@gmail.com', '0');";
