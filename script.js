f4b75e64f168a5c80e172b8d49fc6a777a7bbd4c
LiamBowie <liambowie16+GitHub@gmail.com>
2015-11-18 16:55:11 +0000

    Edited the JQuery

--- script.js
+++ script.js
@@ -3,25 +3,22 @@ $(document).ready(function(){
 	// Add smooth scrolling to all links in navbar + footer link
 	$(".navbar a, footer a[href='#myPage']").on('click', function(event) {
 
-	// Prevent default anchor click behavior
-	event.preventDefault();
+		// Prevent default anchor click behavior
+		event.preventDefault();
 
-	// Store hash
-	var hash = this.hash;
+		// Store hash
+		var hash = this.hash;
 
-	// Using jQuery's animate() method to add smooth page scroll
-	// The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-	$('html, body').animate({
-		scrollTop: $(hash).offset().top
-	}, 900, function(){
+		// Using jQuery's animate() method to add smooth page scroll
+		// The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+		$('html, body').animate({
+			scrollTop: $(hash).offset().top
+		}, 900, function(){
 
-	// Add hash (#) to URL when done scrolling (default click behavior)
-	window.location.hash = hash;
-    });
-  });
-});
-
-$(document).ready(function () {
+			// Add hash (#) to URL when done scrolling (default click behavior)
+			window.location.hash = hash;
+		});
+	});
 
 	var menu = $('.menu');
 	var origOffsetY = menu.offset().top;
@@ -39,5 +36,10 @@ $(document).ready(function () {
 	}
 
 	document.onscroll = scroll;
+});
+
+$(document).ready(function () {
+
+
 
 });
