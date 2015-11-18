4ffb7c0bf447ffce86d940b10f7777bdf0f017ee
LiamBowie <liambowie16+GitHub@gmail.com>
2015-11-18 17:18:04 +0000

    Edited Script

--- adventure.php
+++ adventure.php
@@ -19,10 +19,10 @@
 
             function scroll() {
                 if ($(window).scrollTop() >= origOffsetY) {
-                    $('.sticky-sidebar').addClass('navbar-fixed-top');
+                    $('menu').addClass('navbar-fixed-top');
                     $('.content').addClass('menu-padding');
                 } else {
-                    $('.sticky-sidebar').removeClass('navbar-fixed-top');
+                    $('menu').removeClass('navbar-fixed-top');
                     $('.content').removeClass('menu-padding');
                 }
             }
