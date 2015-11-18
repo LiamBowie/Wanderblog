c4a8fc151305ea49590046f40dd6786ec1da7424
Jo Beattie <beattiejo1@gmail.com>
2015-11-18 15:03:14 +0000

    undid previous changes and changed variable name

--- user.php
+++ user.php
@@ -6,6 +6,8 @@
             ini_set('display_startup_errors', 1);
             error_reporting(E_ALL);
             include 'connect.php';
+
+
         ?>
     </head>
 
--- welcome.php
+++ welcome.php
@@ -148,8 +148,8 @@
 	/* PHP FOR REGISTER */
 		//if($_POST['passwordconfirm'] == $_POST['password']){
 			$sql= "INSERT INTO User VALUES(" . "'" . $_POST['userID'] . "', '" . $_POST['password'] . "', '" . $_POST['firstName'] . "', '" . $_POST['lastName'] . "', '" . $_POST['email'] . "', 0);";
-			$sql = "INSERT INTO User VALUES('jordannbeattie', 'Welcome1', 'Jo', 'Beattie', 'beattiejo1@gmail.com', '0');";
-			$results=mysqli_query($conn, $query);
+			//$sql = "INSERT INTO User VALUES('jordannbeattie', 'Welcome1', 'Jo', 'Beattie', 'beattiejo1@gmail.com', '0');";
+			$results=mysqli_query($conn, $sql);
 			header("Location: user.php");
 		//}
 	?>
