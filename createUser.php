c0653b558c86933704fe2f97687f37b082c93342
Jo Beattie <beattiejo1@gmail.com>
2015-11-18 16:04:28 +0000

    removed variable

--- createUser.php
+++ createUser.php
@@ -1,7 +1,7 @@
 <?php
     include'connect.php';
     //if($_POST['passwordconfirm'] == $_POST['password']){
-    $userID = $users_name = mysqli_real_escape_string($_POST['userID']);
+    $userID =  mysqli_real_escape_string($_POST['userID']);
     $sql= "INSERT INTO User VALUES(" . "'" . $userID . "', '" . $_POST['password'] . "', '" . $_POST['firstName'] . "', '" . $_POST['lastName'] . "', '" . $_POST['email'] . "', 0);";
     //$sql = "INSERT INTO User VALUES('jordannorman', 'Welcome1', 'Jo', 'Beattie', 'beattiejo1@gmail.com', '0');";
     $results=mysqli_query($conn, $sql);
