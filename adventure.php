0055a9623af83be8cb15e6d2f4c09fe07800b9fc
LiamBowie <liambowie16+GitHub@gmail.com>
2015-11-18 17:49:06 +0000

    Added Smooth Scrolling

--- adventure.php
+++ adventure.php
@@ -12,14 +12,26 @@
     <!-- Personal links -->
     <link rel="stylesheet" href="style.css">
     <link rel="shortcut icon" href="images/earth.ico">
-    <script type="text/javascript" src="script.js">
+    <script type="text/javascript">
         $(document).ready(function(){
-            var $window = $(window),
-                $stickyEl = $('#sticky-sidebar'),
-                elTop = $stickyEl.offset().top;
+            // Add smooth scrolling to all links in navbar + footer link
+            $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
 
-            $window.scroll(function() {
-                $stickyEl.toggleClass('navbar-fixed-top', $window.scrollTop() > elTop);
+                // Prevent default anchor click behavior
+                event.preventDefault();
+
+                // Store hash
+                var hash = this.hash;
+
+                // Using jQuery's animate() method to add smooth page scroll
+                // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 900, function(){
+
+                    // Add hash (#) to URL when done scrolling (default click behavior)
+                    window.location.hash = hash;
+                });
             });
         });
     </script>
