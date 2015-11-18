d92a3d06fb093b70bc6026b3ae46da97ec0905f2
Jo Beattie <beattiejo1@gmail.com>
2015-11-18 15:37:35 +0000

    included connect and closed connection

--- createUser.php
+++ createUser.php
@@ -1,8 +1,10 @@
 <?php
+    include'connect.php';
     //if($_POST['passwordconfirm'] == $_POST['password']){
     //$sql= "INSERT INTO User VALUES(" . "'" . $_POST['userID'] . "', '" . $_POST['password'] . "', '" . $_POST['firstName'] . "', '" . $_POST['lastName'] . "', '" . $_POST['email'] . "', 0);";
-    $sql = "INSERT INTO User VALUES('beattiejo1', 'Welcome1', 'Jo', 'Beattie', 'beattiejo1@gmail.com', '0');";
+    $sql = "INSERT INTO User VALUES('jordannorman', 'Welcome1', 'Jo', 'Beattie', 'beattiejo1@gmail.com', '0');";
     $results=mysqli_query($conn, $sql);
-    header("Location: user.php?u=unknown");
+    mysqli_close($conn);
+    header("Location: user.php?u=" . $_POST['userID']);
     //}
 ?>
