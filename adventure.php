9f5b967617d7757d0ae1fb4a5003013b3298aef7
LiamBowie <liambowie16+GitHub@gmail.com>
2015-11-18 18:20:35 +0000

    Moved smooth scrolling js to script.js and added a link the script.js into adventure

--- adventure.php
+++ adventure.php
@@ -12,29 +12,7 @@
     <!-- Personal links -->
     <link rel="stylesheet" href="style.css">
     <link rel="shortcut icon" href="images/earth.ico">
-    <script type="text/javascript">
-        $(document).ready(function(){
-            // Add smooth scrolling to all links in navbar + footer link
-            $(".sticky-sidebar a").on('click', function(event) {
-
-                // Prevent default anchor click behavior
-                event.preventDefault();
-
-                // Store hash
-                var hash = this.hash;
-
-                // Using jQuery's animate() method to add smooth page scroll
-                // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-                $('html, body').animate({
-                    scrollTop: $(hash).offset().top
-                }, 900, function(){
-
-                    // Add hash (#) to URL when done scrolling (default click behavior)
-                    window.location.hash = hash;
-                });
-            });
-        });
-    </script>
+    <script type="text/javascript" src="script.js"></script>
 
     <style>
         /* Set height of the grid so .sidenav can be 100% (adjust if needed) */
