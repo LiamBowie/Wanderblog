eb151c493bade6977a276405986ea4851add6cdd
Jo Beattie <beattiejo1@gmail.com>
2015-11-18 16:11:44 +0000

    used local variables in insertt

--- createUser.php
+++ createUser.php
@@ -2,8 +2,8 @@
     include'connect.php';
     //if($_POST['passwordconfirm'] == $_POST['password']){
     $userID =  mysqli_real_escape_string($_POST['userID']);
-    $sql= "INSERT INTO User VALUES(" . "'" . $userID . "', '" . $_POST['password'] . "', '" . $_POST['firstName'] . "', '" . $_POST['lastName'] . "', '" . $_POST['email'] . "', 0);";
-    //$sql = "INSERT INTO User VALUES('jordannorman', 'Welcome1', 'Jo', 'Beattie', 'beattiejo1@gmail.com', '0');";
+    //$sql= "INSERT INTO User VALUES(" . "'" . $userID . "', '" . $_POST['password'] . "', '" . $_POST['firstName'] . "', '" . $_POST['lastName'] . "', '" . $_POST['email'] . "', 0);";
+    $sql = "INSERT INTO User VALUES('jordannorman', 'Welcome1', 'Jo', 'Beattie', 'beattiejo1@gmail.com', '0');";
     $results=mysqli_query($conn, $sql);
     mysqli_close($conn);
     header("Location: user.php?u=" . $_POST['userID']);
