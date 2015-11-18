0dea863c956a8cf416214be26160d0b0cb9c7c7b
Jo Beattie <beattiejo1@gmail.com>
2015-11-18 15:07:29 +0000

    made createUser.php file for all php from the reg

---
+++ createUser.php
@@ -0,0 +1,8 @@
+<?php
+    //if($_POST['passwordconfirm'] == $_POST['password']){
+    //$sql= "INSERT INTO User VALUES(" . "'" . $_POST['userID'] . "', '" . $_POST['password'] . "', '" . $_POST['firstName'] . "', '" . $_POST['lastName'] . "', '" . $_POST['email'] . "', 0);";
+    $sql = "INSERT INTO User VALUES('jordannbeattie', 'Welcome1', 'Jo', 'Beattie', 'beattiejo1@gmail.com', '0');";
+    $results=mysqli_query($conn, $sql);
+    header("Location: user.php");
+    //}
+?>
