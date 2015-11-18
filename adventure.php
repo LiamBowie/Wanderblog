b4f783c70bc9e7927ad73cf62850cf4ab755aa94
LiamBowie <liambowie16+GitHub@gmail.com>
2015-11-18 15:31:34 +0000

    Edited the blog part of the adventure page

--- adventure.php
+++ adventure.php
@@ -80,37 +80,37 @@
 <div class="container-fluid">
     <div class="row content">
         <div class="col-sm-3 sidenav">
-            <h4>ADVENTURE BLOG</h4>
+            <h4 class="text-center">LOCATION</h4>
             <img src="Images/jo.jpg" class="img-thumbnail img-responsive" alt="Adventure Photo">
             <h5><span class="glyphicon glyphicon-time"></span> Post by *USER*, Sep 27, 2015.</h5>
             <ul class="nav nav-pills nav-stacked">
-                <li class="active"><a href="#section1">Adventure Bio</a></li>
-                <li><a href="#section2">Photos</a></li>
-                <li><a href="#section3">Comments</a></li>
+                <li class="active"><a href="#">Adventure Description</a></li>
+                <li><a href="#photo">Photos</a></li>
+                <li><a href="#comments">Comments</a></li>
             </ul><br>
             <div class="input-group">
                 <input type="text" class="form-control" placeholder="Search Blog..">
-        <span class="input-group-btn">
-          <button class="btn btn-default" type="button">
-              <span class="glyphicon glyphicon-search"></span>
-          </button>
+                <span class="input-group-btn">
+                <button class="btn btn-default" type="button">
+                    <span class="glyphicon glyphicon-search"></span>
+                </button>
         </span>
             </div>
         </div>
 
         <div class="col-sm-9">
-            <h4><small>Adventure Blog</small></h4>
+            <h4><small>POST</small></h4>
             <hr>
             <h2>I Love Food</h2>
-            <h5><span class="label label-danger">Food</span> <span class="label label-primary">Ipsum</span></h5><br>
-            <p>Food is my passion. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <h5><span class="label label-danger">TAG</span> <span class="label label-primary">TAG</span></h5><br>
+            <p>Here is where you would describe your boring ass adventure to the supermarket to get your shitty groceries.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             <br><br>
 
-            <h4><small>RECENT POSTS</small></h4>
+            <h4><small>PHOTOS</small></h4>
             <hr>
-            <h2>Officially Blogging</h2>
-            <h5><span class="glyphicon glyphicon-time"></span> Post by John Doe, Sep 24, 2015.</h5>
-            <h5><span class="label label-success">Lorem</span></h5><br>
+            <h2>Check out these photos of my adventure to LOCATION</h2>
+            <h5><span class="label label-success">TAG</span></h5><br>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             <hr>
 
