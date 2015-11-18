4ad8b349bedf53b26ba29418b72ba2aad263269c
LiamBowie <liambowie16+GitHub@gmail.com>
2015-11-18 18:04:19 +0000

    removed php

--- welcome.php
+++ welcome.php
@@ -67,30 +67,6 @@
 		</div>
 	</nav>
 
-	<?php
-	/*PHP FOR LOGIN */
-		$tryUsername = $_POST['usernameInput'];
-		$tryPassword = $_POST['passwordInput'];
-		$found = false;
-		$query="SELECT * FROM User;";
-		$results=mysqli_query($conn, $query);
-
-		if(mysqli_num_rows($results)>0) { /* if there are results (rows>0) */
-			while (($row = mysqli_fetch_array($results)) && ($found == false)) {
-				if($tryUsername==$row['userID'] && $tryPassword==$row['password']){
-					$found=true;
-					$username=$tryUsername;
-					session_start();
-					$_SESSION['username']=$username;
-					$_SESSION['access_level']='standard_user';
-					header("Location: https://google.com");
-				}
-			}
-		}
-
-
-	?>
-
 	<!-- Modal -->
 	<div class="modal fade" id="modal-reg" role="dialog">
 		<div class="modal-dialog">
