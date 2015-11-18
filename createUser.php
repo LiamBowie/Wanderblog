4557c657828bf4d2c8a67b5e5485deb792ceb4f8
Jo Beattie <beattiejo1@gmail.com>
2015-11-18 15:38:37 +0000

    updated query

--- createUser.php
+++ createUser.php
@@ -1,8 +1,8 @@
 <?php
     include'connect.php';
     //if($_POST['passwordconfirm'] == $_POST['password']){
-    //$sql= "INSERT INTO User VALUES(" . "'" . $_POST['userID'] . "', '" . $_POST['password'] . "', '" . $_POST['firstName'] . "', '" . $_POST['lastName'] . "', '" . $_POST['email'] . "', 0);";
-    $sql = "INSERT INTO User VALUES('jordannorman', 'Welcome1', 'Jo', 'Beattie', 'beattiejo1@gmail.com', '0');";
+    $sql= "INSERT INTO User VALUES(" . "'" . $_POST['userID'] . "', '" . $_POST['password'] . "', '" . $_POST['firstName'] . "', '" . $_POST['lastName'] . "', '" . $_POST['email'] . "', 0);";
+    //$sql = "INSERT INTO User VALUES('jordannorman', 'Welcome1', 'Jo', 'Beattie', 'beattiejo1@gmail.com', '0');";
     $results=mysqli_query($conn, $sql);
     mysqli_close($conn);
     header("Location: user.php?u=" . $_POST['userID']);
