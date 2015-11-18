9586b0bb5d955da38ea0f6d752d1467412386947
LiamBowie <liambowie16+GitHub@gmail.com>
2015-11-18 17:05:34 +0000

    Adventure Page should be linked to the script file

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
 $(document).ready(function(){
+	var menu = $('.menu');
+	var origOffsetY = menu.offset().top;
 
-	// Add smooth scrolling to all links in navbar + footer link
-	$(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+	function scroll() {
+		if ($(window).scrollTop() >= origOffsetY) {
+			$('.menu').addClass('sticky');
+			$('.content').addClass('menu-padding');
+		} else {
+			$('.menu').removeClass('sticky');
+			$('.content').removeClass('menu-padding');
+		}
+	}
 
-		// Prevent default anchor click behavior
-		event.preventDefault();
-
-		// Store hash
-		var hash = this.hash;
-
-		// Using jQuery's animate() method to add smooth page scroll
-		// The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-		$('html, body').animate({
-			scrollTop: $(hash).offset().top
-		}, 900, function(){
-
-			// Add hash (#) to URL when done scrolling (default click behavior)
-			window.location.hash = hash;
-		});
-	});
+	document.onscroll = scroll;
 });
