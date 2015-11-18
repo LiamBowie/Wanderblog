6d7b66f1ba797a026960938e9b00ca52c3437012
Jo Beattie <beattiejo1@gmail.com>
2015-11-18 15:48:15 +0000

    added real escape

--- createUser.php
+++ createUser.php
@@ -1,7 +1,8 @@
 <?php
     include'connect.php';
     //if($_POST['passwordconfirm'] == $_POST['password']){
-    $sql= "INSERT INTO User VALUES(" . "'" . $_POST['userID'] . "', '" . $_POST['password'] . "', '" . $_POST['firstName'] . "', '" . $_POST['lastName'] . "', '" . $_POST['email'] . "', 0);";
+    $userID = $users_name = mysqli_real_escape_string($_POST['userID']);
+    $sql= "INSERT INTO User VALUES(" . "'" . $userID . "', '" . $_POST['password'] . "', '" . $_POST['firstName'] . "', '" . $_POST['lastName'] . "', '" . $_POST['email'] . "', 0);";
     //$sql = "INSERT INTO User VALUES('jordannorman', 'Welcome1', 'Jo', 'Beattie', 'beattiejo1@gmail.com', '0');";
     $results=mysqli_query($conn, $sql);
     mysqli_close($conn);
