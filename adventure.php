1d65702e5bd87f20f6173280ed909d219c6e7a51
LiamBowie <liambowie16+GitHub@gmail.com>
2015-11-18 14:54:00 +0000

    testing the azure local git

--- adventure.php
+++ adventure.php
@@ -43,13 +43,13 @@
 <div class="container-fluid">
     <div class="row content">
         <div class="col-sm-3 sidenav">
-            <h4>John's Blog</h4>
-            <img src="Images/jo.jpg" class="img-thumbnail img-responsive" alt="Profile Photo">
+            <h4>Adventure Title TEST</h4>
+            <img src="Images/jo.jpg" class="img-thumbnail img-responsive" alt="Adventure Photo">
+            <h5><span class="glyphicon glyphicon-time"></span> Post by *USER*, Sep 27, 2015.</h5>
             <ul class="nav nav-pills nav-stacked">
-                <li class="active"><a href="#section1">Home</a></li>
-                <li><a href="#section2">Friends</a></li>
-                <li><a href="#section3">Family</a></li>
-                <li><a href="#section3">Photos</a></li>
+                <li class="active"><a href="#section1">Adventure Bio</a></li>
+                <li><a href="#section2">Photos</a></li>
+                <li><a href="#section3">Comments</a></li>
             </ul><br>
             <div class="input-group">
                 <input type="text" class="form-control" placeholder="Search Blog..">
@@ -62,10 +62,9 @@
         </div>
 
         <div class="col-sm-9">
-            <h4><small>RECENT POSTS</small></h4>
+            <h4><small>Adventure Blog</small></h4>
             <hr>
             <h2>I Love Food</h2>
-            <h5><span class="glyphicon glyphicon-time"></span> Post by Jane Dane, Sep 27, 2015.</h5>
             <h5><span class="label label-danger">Food</span> <span class="label label-primary">Ipsum</span></h5><br>
             <p>Food is my passion. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             <br><br>
