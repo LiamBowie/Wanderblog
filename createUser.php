ae0198b1f57ec49a1e71934450c0a2cc327b7626
Jo Beattie <beattiejo1@gmail.com>
2015-11-18 16:22:56 +0000

    try input again

--- createUser.php
+++ createUser.php
@@ -3,8 +3,8 @@
     //if($_POST['passwordconfirm'] == $_POST['password']){
     $userID=$_POST['userID'];
     //$userID =  mysqli_real_escape_string($_POST['userID']);
-    echo $userID;
-    //$sql= "INSERT INTO User VALUES(" . "'" . $userID . "', '" . $_POST['password'] . "', '" . $_POST['firstName'] . "', '" . $_POST['lastName'] . "', '" . $_POST['email'] . "', 0);";
+    //echo $userID;
+    $sql= "INSERT INTO User VALUES(" . "'" . $userID . "', '" . $_POST['password'] . "', '" . $_POST['firstName'] . "', '" . $_POST['lastName'] . "', '" . $_POST['email'] . "', 0);";
     //$sql = "INSERT INTO User VALUES('jordannorman', 'Welcome1', 'Jo', 'Beattie', 'beattiejo1@gmail.com', '0');";
     $results=mysqli_query($conn, $sql);
     mysqli_close($conn);
