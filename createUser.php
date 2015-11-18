51cf73327b09c9b1b6fea27fd901969751aa2e05
Jo Beattie <beattiejo1@gmail.com>
2015-11-18 16:27:07 +0000

    added value

--- createUser.php
+++ createUser.php
@@ -1,13 +1,11 @@
 <?php
     include'connect.php';
     //if($_POST['passwordconfirm'] == $_POST['password']){
-    $userID=$_POST['userID'];
-    //$userID =  mysqli_real_escape_string($_POST['userID']);
-    //echo $userID;
-    $sql= "INSERT INTO User VALUES(" . "'" . $userID . "', '" . $_POST['password'] . "', '" . $_POST['firstName'] . "', '" . $_POST['lastName'] . "', '" . $_POST['email'] . "', 0);";
-    //$sql = "INSERT INTO User VALUES('jordannorman', 'Welcome1', 'Jo', 'Beattie', 'beattiejo1@gmail.com', '0');";
-    $results=mysqli_query($conn, $sql);
-    mysqli_close($conn);
-    //header("Location: user.php?u=" . $_POST['userID']);
+        $userID=$_POST['userID'];
+        $sql= "INSERT INTO User VALUES(" . "'" . $userID . "', '" . $_POST['password'] . "', '" . $_POST['firstName'] . "', '" . $_POST['lastName'] . "', '" . $_POST['email'] . "', 0);";
+        //$sql = "INSERT INTO User VALUES('jordannorman', 'Welcome1', 'Jo', 'Beattie', 'beattiejo1@gmail.com', '0');";
+        $results=mysqli_query($conn, $sql);
+        mysqli_close($conn);
+        header("Location: user.php?u=" . $_POST['userID']);
     //}
 ?>
