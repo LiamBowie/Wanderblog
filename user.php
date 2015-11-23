3a5b74ad2e758abfa5451133653bb419e50d7f26
Jo Beattie <beattiejo1@gmail.com>
2015-11-23 13:39:46 +0000

    removed session_start

--- user.php
+++ user.php
@@ -1,6 +1,5 @@
 <html>
     <head>
-        <?php session_start(); ?>
         <title>Created User</title>
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
