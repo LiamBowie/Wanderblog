3fc6d8087efae8f62403e4b7e91978cd475a8693
Jo Beattie <beattiejo1@gmail.com>
2015-11-30 13:17:06 +0000

    update

--- display.php
+++ display.php
@@ -1,4 +1,7 @@
 <?php
+
+session_start();
+
 $output="";
 //if(isset($_SESSION['loggedIn'])) {
     $output = $output . "User: " . $_SESSION['FullName'] . "<br>" . "Logged In: " . $_SESSION['loggedIn'];
