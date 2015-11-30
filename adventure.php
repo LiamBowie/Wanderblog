cf7ba839543ae2235467715035201b67aec1b92e
Jo Beattie <beattiejo1@gmail.com>
2015-11-30 16:46:52 +0000

    added navbar with include

--- adventure.php
+++ adventure.php
@@ -43,41 +43,7 @@
 </head>
 <body style="padding-top: 75px;" data-spy="scroll" data-target=".sticky-sidebar" data-offset="50">
 
-<nav class="navbar navbar-default navbar-fixed-top">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="welcome.php"><span class="glyphicon glyphicon-globe"></span></a>
-        </div>
-        <div class="collapse navbar-collapse" id="myNavbar">
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="welcome.php#top5">TOP 5 TRIPS</a></li>
-                <li><a href="#" data-toggle="modal" data-target="#modal-reg">REGISTER</a></li>
-                <li>
-                    <form role="form" style="padding-top: 10px" class="form-inline" action="welcome.php" method="post">
-                        <input class="form-control" type="text" name="usernameInput" placeholder="Username">
-                        <input class="form-control" type="password" name="passwordInput" placeholder="Password">
-                        <button type="submit" class="btn btn-success">LOGIN</button>
-                    </form>
-                </li>
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">MORE
-                        <span class="caret"></span>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a href="about.php">ABOUT</a></li>
-                        <li><a href="adventuresearch.html">ADVENTURE SEARCH</a></li>
-                        <li><a href="authorsearch.html">AUTHOR SEARCH</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+<?php include'navbar.php'; ?>
 
 <div class="container-fluid">
     <div class="row content">
