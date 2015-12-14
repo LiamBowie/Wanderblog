326c0e0f04536816749144782261947b573dbd8a
LiamBowie <liambowie16+GitHub@gmail.com>
2015-12-14 23:54:29 +0000

    Removed Comment Section and renamed some other sections also edited sidebar to accomodate for the changes

--- author.php
+++ author.php
@@ -53,72 +53,29 @@ include 'connect.php';
     <div class="row content">
         <div class="col-sm-3 sidenav">
             <h4 class="text-center"><?php echo $row['location'] ?></h4>
-            <img src= "<?php echo $photoPath; ?>" class="img-thumbnail img-responsive" alt="Adventure Photo">
+            <img src= "<?php echo $photoPath; ?>" class="img-thumbnail img-responsive" alt="Author Photo">
             <h5><span class="glyphicon glyphicon-time"></span> Post by <a href=<?php echo $authorPath; ?> > <?php echo $row['authorName'] ?></a></h5>
             <nav class="sticky-sidebar">
                 <ul class="nav nav-pills nav-stacked">
-                    <li><a href="#desc">Adventure Description</a></li>
-                    <li><a href="#photos">Photos</a></li>
-                    <li><a href="#comments">Comments</a></li>
+                    <li><a href="#desc">Author Bio</a></li>
+                    <li><a href="#photos">Adventures</a></li>
                 </ul><br>
             </nav>
         </div>
 
 
         <div class="col-sm-9">
-            <h2 id="desc" class="anchor"><?php echo $row['title']; ?></h2>
+            <h2 id="desc" class="anchor">Bio</h2>
             <hr>
             <h5><span class="label label-danger">TAG</span> <span class="label label-primary">TAG</span></h5><br>
             <p><?php echo $row['content'] ?></p>
             <br><br>
 
-            <h4 id="photos" class="anchor"><small>PHOTOS</small></h4>
+            <h4 id="photos" class="anchor"><small>Adventures</small></h4>
             <hr>
-            <h2>Check out these photos of my adventure to LOCATION</h2>
-            <p><strong>GRID OF IMAGES. IMPLEMENT LATER. TOO LAZY JUST NOW.</strong></p>
+            <h2>Check out all these dank ass adventures I've been on!</h2>
 
-            <h4 id="comments" class="anchor"><small>COMMENTS</small></h4>
-            <hr>
-            <h4>Leave a Comment:</h4>
-            <form role="form">
-                <div class="form-group">
-                    <textarea class="form-control" rows="3" required></textarea>
-                </div>
-                <button type="submit" class="btn btn-success">Submit</button>
-            </form>
-            <br><br>
 
-            <p><span class="badge">2</span> Comments:</p><br>
-
-            <div class="row">
-                <div class="col-sm-2 text-center">
-                    <img src="Images/jo.jpg" class="img-circle" height="65" width="65" alt="Avatar">
-                </div>
-                <div class="col-sm-10">
-                    <h4>Anja <small>Sep 29, 2015, 9:12 PM</small></h4>
-                    <p>Keep up the GREAT work! I am cheering for you!! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <br>
-                </div>
-                <div class="col-sm-2 text-center">
-                    <img src="Images/jo.jpg" class="img-circle" height="65" width="65" alt="Avatar">
-                </div>
-                <div class="col-sm-10">
-                    <h4>John Row <small>Sep 25, 2015, 8:25 PM</small></h4>
-                    <p>I am so happy for you man! Finally. I am looking forward to read about your trendy life. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <br>
-                    <p><span class="badge">1</span> Comment:</p><br>
-                    <div class="row">
-                        <div class="col-sm-2 text-center">
-                            <img src="Images/jo.jpg" class="img-circle" height="65" width="65" alt="Avatar">
-                        </div>
-                        <div class="col-xs-10">
-                            <h4>Nested Bro <small>Sep 25, 2015, 8:28 PM</small></h4>
-                            <p>Me too! WOW!</p>
-                            <br>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
 </div>
