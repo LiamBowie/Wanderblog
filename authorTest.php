cfe46c173a1c4f93234c7476b80379b8b47f1c4b
Jo Beattie <beattiejo1@gmail.com>
2015-12-15 22:01:12 +0000

    removed all statements

--- authorTest.php
+++ authorTest.php
@@ -10,11 +10,12 @@ $results = mysqli_query($conn, $query);
 
 // Gets the row from the created table above
 $row = mysqli_fetch_array($results);
-
+echo $row['title'];
+/*
 if (mysqli_num_rows($results) > 0){
     while ($row = mysqli_fetch_array($results) != 0){
         echo  $row['title'];
     }
-}
+} */
 
 //while ($row = mysqli_fetch_array($result2, MYSQLI_BOTH))
