8e878c853f0389aeba601d1be3fb5567e544fee5
Jo Beattie <beattiejo1@gmail.com>
2015-12-15 22:03:22 +0000

    loop with while to ouput title

--- authorTest.php
+++ authorTest.php
@@ -10,7 +10,9 @@ $results = mysqli_query($conn, $query);
 
 // Gets the row from the created table above
 $row = mysqli_fetch_array($results);
-echo $row['title'];
+while ($row = mysqli_fetch_array($result2, MYSQLI_BOTH)){
+    echo $row['title'];
+}
 /*
 if (mysqli_num_rows($results) > 0){
     while ($row = mysqli_fetch_array($results) != 0){
