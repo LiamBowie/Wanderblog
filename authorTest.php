d0ccf194dea18a2019ca0b9cf54c694cc3b1fcd6
Jo Beattie <beattiejo1@gmail.com>
2015-12-15 22:27:02 +0000

    revert! revert!

--- authorTest.php
+++ authorTest.php
@@ -3,13 +3,29 @@
 
 
 $query = "SELECT * FROM adventure WHERE author = '" . $_GET['auth'] . "';";
-$results = mysqli_query($conn, $query);
-$row = mysqli_fetch_array($results);
-$found = false;
 
+// Creates result table from query
+$results = mysqli_query($conn, $query);
+
+
+// Gets the row from the created table above
+$row = mysqli_fetch_array($results);
+
+/*
+if (mysqli_num_rows($results) > 0){
+    while ($row = mysqli_fetch_array($results) != 0){
+        echo  $row['title'];
+    }
+} */
+
+//while ($row = mysqli_fetch_array($result2, MYSQLI_BOTH))
+
+$found = false;
+//$query = 'Select CONCAT(firstName, " ", lastName) AS FullName, userID, emailAddress, password, isAdmin FROM User;';
+$results = mysqli_query($conn, $query);
 
 if (mysqli_num_rows($results) > 0) { /* if there are results (rows>0) */
     while (($row = mysqli_fetch_array($results)) && ($found == false)) {
-        echo $row['title']  . "<br>";
+        echo $row['title'] . "<br>";
     }
 }
