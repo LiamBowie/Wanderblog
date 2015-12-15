5743c85b926b8600f240324970b0a0db45fea792
LiamBowie <liambowie16+GitHub@gmail.com>
2015-12-15 15:09:29 +0000

    Trying something different in the Title tags.

--- author.php
+++ author.php
@@ -1,7 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Adventure</title>
+    <title>
+        <?php
+            if ($row['fullName'] == null)
+            {
+                echo "Author Page";
+            }
+            else
+            {
+                echo $row['fullName'];
+            }
+        ?>
+    </title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
