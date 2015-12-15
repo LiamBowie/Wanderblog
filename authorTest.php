0b491b2d3ef9213e95abaeee4d2646fc85e1e5ec
Jo Beattie <beattiejo1@gmail.com>
2015-12-15 22:12:50 +0000

    added line break

--- authorTest.php
+++ authorTest.php
@@ -26,6 +26,6 @@ $results = mysqli_query($conn, $query);
 
 if (mysqli_num_rows($results) > 0) { /* if there are results (rows>0) */
     while (($row = mysqli_fetch_array($results)) && ($found == false)) {
-        echo $row[''] . " " . $row['password'] . "\n";
+        echo $row[''] . " " . $row['password'] . "<br>";
     }
 }
