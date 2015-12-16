e85546106a00ed805c8ade4d3c149241869c8fc2
finlay54 <finlayjohnston60@gmail.com>
2015-12-16 11:31:09 +0000

    adding correct version of search

--- search.php
+++ search.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <?php include'connect.php'; ?> /* include connection to db */
+    <?php include'connect.php'; ?>
     <style>
         .navbar {
             margin-bottom: 0;
@@ -67,19 +67,33 @@
             <p><a href="#">Link</a></p>
         </div>
         <div class="col-sm-8 text-left">
-            <h1>Welcome</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+            <h1>Displaying results for *searchvalue*</h1>
+
             <hr>
-            <h3>Test</h3>
-            <p>Lorem ipsum...</p>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <a href="#" class="thumbnail">
+                            <img src="http://bit.ly/1m6Q0nF">
+                            <p>Insert Information here</p>
+                        </a>
+                    </div>
+                    <div class="col-sm-4">
+                        <a href="#" class="thumbnail">
+                            <img src="http://bit.ly/1m6Q0nF">
+                            <p>Insert Information here</p>
+                        </a>
+                    </div>
+                    <div class="col-sm-4">
+                        <a href="#" class="thumbnail">
+                            <img src="http://bit.ly/1m6Q0nF">
+                            <p>Insert Information here</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="col-sm-2 sidenav">
-            <div class="well">
-                <p>ADS</p>
-            </div>
-            <div class="well">
-                <p>ADS</p>
-            </div>
         </div>
     </div>
 </div>
