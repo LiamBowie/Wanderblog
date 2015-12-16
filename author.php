490bdd565558c0b4e85eff8bf1717625683de41e
Jo Beattie <beattiejo1@gmail.com>
2015-12-16 13:29:09 +0000

    shortened name column to 3

--- author.php
+++ author.php
@@ -99,7 +99,7 @@ function showEdit(){
 
         <div class="col-sm-9">
             <div class="row">
-                <div class="col-sm-2"> <h2 id="bio" class="anchor"><?php echo $row['fullName'] ?></h2> </div>
+                <div class="col-sm-3"> <h2 id="bio" class="anchor"><?php echo $row['fullName'] ?></h2> </div>
                 <div class="col-s-4"> <p><?php showEdit() ?></p> </div>
             </div>
             <hr>
