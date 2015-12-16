869128a7789c9c132535c32d7b5bb3003c2c8834
Jo Beattie <beattiejo1@gmail.com>
2015-12-16 02:32:29 +0000

    createUser updated to enter details into Author table if box is checked

--- createUser.php
+++ createUser.php
@@ -1,15 +1,36 @@
 <?php
     include'connect.php';
 
+    $found = false;
+    $query = 'SELECT * FROM Author';
+    $results = mysqli_query($conn, $query);
+    $last = "";
+    if (mysqli_num_rows($results) > 0) { /* if there are results (rows>0) */
+        while (($row = mysqli_fetch_array($results)) && ($found == false)) {
+            $last = $row['authorID'];
+        }
+    }
+    $lastArray = str_split($last);
+    $lastNum = (int)$lastArray[8];
+    $newNum = $lastNum + 1;
+    $newID = "";
+    for ($i = 0; $i < 8; $i++) {
+        $newID = $newID . $lastArray[$i];
+    }
+    $newID = $newID . $newNum;
+
     if($_POST['passwordconfirm'] == $_POST['password']) {
-        if($_POST['authorCheck'] == 1){ $author =1; }
-        else{ $author = 0; }
 
         $username = $_POST['userID'];
-
-        $sql = "INSERT INTO User VALUES('" . $username . "', '" . $_POST['password'] . "', '" . $_POST['firstName'] . "', '" . $_POST['lastName'] . "', '" . $_POST['email'] . "'," . $author . ");";
+        $sql = "INSERT INTO User VALUES('" . $username . "', '" . $_POST['password'] . "', '" . $_POST['firstName'] . "', '" . $_POST['lastName'] . "', '" . $_POST['email'] . "', 0);";
 
         $results = mysqli_query($conn, $sql);
+
+        if($_POST['authorCheck'] == 1) {
+            $photo = "Images/blankAuth.png";
+            $sqlTwo = "INSERT INTO Author VALUES('" . $newID . "', '" . $username . "', " . $photo . ", '" . $username . " is a new author to Wanderblog', 'LO00000');";
+        }
+
         mysqli_close($conn);
 
         session_start();
