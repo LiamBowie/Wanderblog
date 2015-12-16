b2a9c03ec70336f619ee0208acd08ce01780d0f3
Jo Beattie <beattiejo1@gmail.com>
2015-12-16 02:51:13 +0000

    execute query

--- createUser.php
+++ createUser.php
@@ -29,6 +29,7 @@
         if($_POST['authorCheck'] == 1) {
             $photo = "Images/blankAuth.png";
             $sqlTwo = "INSERT INTO Author VALUES('" . $newID . "', '" . $username . "', '" . $photo . "', '" . $username . " is a new author to Wanderblog', 'LO00000');";
+            $resultsTwo = mysqli_query($conn, $sqlTwo);
         }
 
         mysqli_close($conn);
