701bd465e4c99a167ff6bcfda86ff5a85f19be98
Jo Beattie <beattiejo1@gmail.com>
2015-12-16 01:50:48 +0000

    n/a

--- autoGenTest.php
+++ autoGenTest.php
@@ -8,14 +8,12 @@ $results = mysqli_query($conn, $query);
 $last = "";
 
 if (mysqli_num_rows($results) > 0) { /* if there are results (rows>0) */
-    while ($row = mysqli_fetch_array($results)){ //$row = the row you're in
-        $last = $row['authorID'];   //get the latest AuthID
-    } //close while
-} //close if
+    while (($row = mysqli_fetch_array($results)) && ($found == false)) {
+        $last = $row['authorID'];
+    }
+}
 
-$lastArray = str_split($last); //turn the authID into an array
-$toNum = ""; //set variable for going to a number
-foreach($lastArray as $entry){ $toNum = $toNum . $entry; }
+$lastArray = str_split($last);
 $lastNum = (int)$lastArray;
 echo $lastNum;
 $newNum = $lastNum + 1;
