dd58c4631707cace277f76a60e9efadc78a7cbb5
Jo Beattie <beattiejo1@gmail.com>
2015-12-16 00:44:44 +0000

    split string into Array

---
+++ autoGenTest.php
@@ -0,0 +1,20 @@
+<?php
+
+include'connect.php';
+
+$found = false;
+$query = 'SELECT * FROM Author';
+$results = mysqli_query($conn, $query);
+$last = "";
+
+if (mysqli_num_rows($results) > 0) { /* if there are results (rows>0) */
+    while (($row = mysqli_fetch_array($results)) && ($found == false)) {
+        $last = $row['authorID'];
+    }
+}
+
+$lastArray = explode(',',$last, 9);
+
+for($i = 0; $i < 9; $i++){
+    echo $lastArray[$i] + ", ";
+}
