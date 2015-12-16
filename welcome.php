d4ce4186f0d52e5d1882412705fe45e7d334d0f3
LiamBowie <liambowie16+GitHub@gmail.com>
2015-12-16 12:33:18 +0000

    troubleshooting carousel.

--- welcome.php
+++ welcome.php
@@ -76,12 +76,18 @@
 		<ol class="carousel-indicators">
 			<li data-target="#tweetCarousel" data-slide-to="0" class="active"></li>
 			<li data-target="#tweetCarousel" data-slide-to="1"></li>
-			<li data-target="#tweetCarousel" data-slide-to="2"></li>
+			<!--<li data-target="#tweetCarousel" data-slide-to="2"></li>
 			<li data-target="#tweetCarousel" data-slide-to="3"></li>
-			<li data-target="#tweetCarousel" data-slide-to="4"></li>
+			<li data-target="#tweetCarousel" data-slide-to="4"></li>-->
 		</ol>
 
-		<div class="carousel-inner" id="twitter" role="listbox">
+		<div class="carousel-inner" role="listbox">
+			<div class="item active">
+				<h4>"This company is the best. I am so happy with the result!"<br><span style="font-style:normal;">Michael Roe, Vice President, Comment Box</span></h4>
+			</div>
+			<div class="item">
+				<h4>"One word... WOW!!"<br><span style="font-style:normal;">John Doe, Salesman, Rep Inc</span></h4>
+			</div>
 
 		</div>
 
