9d058dedf22df318ab5b5aed5a334a6ebb8ba0a6
Jo Beattie <beattiejo1@gmail.com>
2015-12-16 02:01:29 +0000

    add to newNum

--- autoGenTest.php
+++ autoGenTest.php
@@ -14,8 +14,8 @@ if (mysqli_num_rows($results) > 0) { /* if there are results (rows>0) */
 }
 
 $lastArray = str_split($last);
-//$lastNum = (int)$lastArray;
 $lastNum = (int)$lastArray[8];
-echo $lastNum;
 $newNum = $lastNum + 1;
+$newNum = "";
+for($i=0; $i<8; $i++){ $newNum = $newNum . $lastArray[$i]; }
 echo $newNum;
