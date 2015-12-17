38cb80c3af6910978893cc668c9de772b818d5aa
Jo Beattie <beattiejo1@gmail.com>
2015-12-17 16:32:58 +0000

    removed if

--- commentIDTest.php
+++ commentIDTest.php
@@ -2,21 +2,21 @@
 // Jo's AutoGen for Comment ID
 
 $found = false;                                                             //have not found id
-$query = 'SELECT * FROM Comments';                                          //get all comments
+$query = 'SELECT * FROM Comments;';                                          //get all comments
 $results = mysqli_query($conn, $query);                                     //execute query
 $last = "";                                                                 //last ID
 //if (mysqli_num_rows($results) > 0) { /* if there are results (rows>0) */
     while (($row = mysqli_fetch_array($results)) && ($found == false)) {    //loop results
-        $last = $row['commentID'];
-        echo $last;//get latest ID
-    }
+        $last = $row['commentID'];                                          //get last ID used
+        echo $last;                                                         //output last (for testing)
+    }                                                                       //end while
 //}
-$lastArray = str_split($last);
-$lastNum = (int)$lastArray[9];
-$newNum = $lastNum + 1;
-$newID = "";
-for ($i = 0; $i < 9; $i++) {
-    $newID = $newID . $lastArray[$i];
-}
-$newID = $newID . $newNum;
-echo $newID;
+$lastArray = str_split($last);                                              //split into array
+$lastNum = (int)$lastArray[9];                                              //last number = last entry in array
+$newNum = $lastNum + 1;                                                     //add 1 to last number
+$newID = "";                                                                //create blank newID
+for ($i = 0; $i < 8; $i++) {                                                //loop to 8
+    $newID = $newID . $lastArray[$i];                                       //add elements from last array to new ID
+}                                                                           //end loop
+$newID = $newID . $newNum;                                                  //newID = newID plus new Number
+echo $newID;                                                                //output the new ID
