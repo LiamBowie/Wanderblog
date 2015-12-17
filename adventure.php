cadd8bc4c631fd9be7653e8bff06f3f4641bcb06
LiamBowie <liambowie16+GitHub@gmail.com>
2015-12-17 17:16:27 +0000

    changed to <p> tags

--- adventure.php
+++ adventure.php
@@ -100,7 +100,7 @@
 
         <div class="col-sm-9">
             <h2 id="desc" class="anchor"><?php echo $row['title'];?></h2>
-            <p class="badge">Votes: <?php echo $row['noOfVotes'];?></p>
+            <p>This is a sentence</p>
             <hr>
             <h5><span class="label label-danger">TAG</span> <span class="label label-primary">TAG</span></h5><br>
             <p><?php echo $row['content'] ?></p>
