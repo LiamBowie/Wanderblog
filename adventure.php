4e457282efbdc54053b08f92476ef0e491f702a5
LiamBowie <liambowie16+GitHub@gmail.com>
2015-12-17 16:47:01 +0000

    removed unfinished tag

--- adventure.php
+++ adventure.php
@@ -100,7 +100,7 @@
 
 
         <div class="col-sm-9">
-            <h2 id="desc" class="anchor"><?php echo $row['title'];?><span class="badge"><?php echo $row['noOfVotes'];?></span><button class=""</h2>
+            <h2 id="desc" class="anchor"><?php echo $row['title'];?><span class="badge"><?php echo $row['noOfVotes'];?></span></h2>
             <hr>
             <h5><span class="label label-danger">TAG</span> <span class="label label-primary">TAG</span></h5><br>
             <p><?php echo $row['content'] ?></p>
