b283ed1dff9de3e7091b81e49865314c5ef7192c
LiamBowie <liambowie16+GitHub@gmail.com>
2015-12-17 23:38:54 +0000

    Changed variable in connect

--- connect.php
+++ connect.php
@@ -1,12 +1,12 @@
 <?php
     // SQL Variables for connection
         $servername = 'eu-cdbr-azure-north-d.cloudapp.net';
-        $username = 'b5b914ecf97d5c';
+        $dbusername = 'b5b914ecf97d5c';
         $password = 'ff9cd70a';
         $dbname = 'SHBWanderBlog';
 
     // Connect to DB
-        $conn = mysqli_connect($servername, $username, $password, $dbname);
+        $conn = mysqli_connect($servername, $dbusername, $password, $dbname);
 
     // Display any errors
         if(!$conn){ die("Connection failed: " . mysqli_error($conn));}
