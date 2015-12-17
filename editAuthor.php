c3f9086cafd3590632cfcdacab8ba802ab414fdb
finlay54 <finlayjohnston60@gmail.com>
2015-12-17 22:11:38 +0000

    added end of text area

--- editAuthor.php
+++ editAuthor.php
@@ -110,7 +110,7 @@ function saveChanges(){
                     <?php echo "<h2> . " .  $row['fullName'] . "</h2>" ?>
                 </div>
                 <hr>
-                <?php echo "<p><textarea cols='50' rows='10' type='text' id='bio' name='bio' value='test'></p>"; ?>
+                <?php echo "<p><textarea cols='50' rows='10' type='text' id='bio' name='bio' value='test'></textarea></p>"; ?>
                 <br><br>
             </div>
 
