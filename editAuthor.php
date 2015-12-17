3b83ef1c579714c9ca49b928c284422379c3b059
finlay54 <finlayjohnston60@gmail.com>
2015-12-17 21:41:52 +0000

    Commenting out type=text

--- editAuthor.php
+++ editAuthor.php
@@ -110,7 +110,7 @@ function saveChanges(){
                     <?php echo "<h2> . " .  $row['fullName'] . "</h2>" ?>
                 </div>
                 <hr>
-                <?php echo "<p><textarea cols='50' rows='10' type='text' id='bio' name='bio' value='" . $row['bio'] . "'></p>" ?>
+                <?php echo "<p><textarea cols='50' rows='10' <!--type='text'--> id='bio' name='bio' value='" . $row['bio'] . "'></p>" ?>
                 <br><br>
             </div>
 
