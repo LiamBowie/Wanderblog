f404ff8a5ff1d09dd0a8b2feb1c99ef5c6465c9a
Jo Beattie <beattiejo1@gmail.com>
2015-12-17 20:14:35 +0000

    changed [9] to [8]

--- commentIDTest.php
+++ commentIDTest.php
@@ -4,15 +4,15 @@ include'connect.php';
 $found = false;                                                             //have not found id
 $query = 'SELECT * FROM Comments;';                                          //get all comments
 $results = mysqli_query($conn, $query);                                     //execute query
-$last = "COM000005";                                                                 //last ID
+$last = "";                                                                 //last ID
 //if (mysqli_num_rows($results) > 0) { /* if there are results (rows>0) */
     while (($row = mysqli_fetch_array($results)) && ($found == false)) {    //loop results
         $last = $row['commentID'];                                          //get last ID used
-        echo $last + ", ";                                                         //output last (for testing)
+        //echo $last + ", ";                                                         //output last (for testing)
     }                                                                       //end while
 //}
 $lastArray = str_split($last);                                              //split into array
-$lastNum = (int)$lastArray[9];                                              //last number = last entry in array
+$lastNum = (int)$lastArray[8];                                              //last number = last entry in array
 $newNum = $lastNum + 1;                                                     //add 1 to last number
 $newID = "";                                                                //create blank newID
 for ($i = 0; $i < 9; $i++) {                                                //loop to 8
