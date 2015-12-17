5fd25db464bfae3caa18afeda57890bc0eea5924
Jo Beattie <beattiejo1@gmail.com>
2015-12-17 16:15:54 +0000

    added autoGen

--- addComment.php
+++ addComment.php
@@ -15,6 +15,27 @@ include 'connect.php';
 
     session_start();
 
+// Jo's AutoGen for Comment ID
+
+    $found = false;                                                             //have not found id
+    $query = 'SELECT * FROM Comments';                                          //get all comments
+    $results = mysqli_query($conn, $query);                                     //execute query
+    $last = "";                                                                 //last ID
+    if (mysqli_num_rows($results) > 0) { /* if there are results (rows>0) */
+        while (($row = mysqli_fetch_array($results)) && ($found == false)) {    //loop results
+            $last = $row['commentID'];                                          //get latest ID
+        }
+    }
+    $lastArray = str_split($last);
+    $lastNum = (int)$lastArray[9];
+    $newNum = $lastNum + 1;
+    $newID = "";
+    for ($i = 0; $i < 9; $i++) {
+        $newID = $newID . $lastArray[$i];
+    }
+    $newID = $newID . $newNum;
+    echo $newID;
+
     $comment = $_POST['comment-text'];
     $userid = $_SESSION['username'];
     $advid = $_GET['adv'];
@@ -27,5 +48,5 @@ echo "U: " . $userid . " C: " . $comment . " A: " . $advid;
     $results = mysqli_query($conn, $sql);
     mysqli_close($conn);
 
-    header("Location: adventure.php?adv=" . $advid . "#comments");
+    //header("Location: adventure.php?adv=" . $advid . "#comments");
 ?>
