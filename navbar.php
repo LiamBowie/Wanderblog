5448ad2d10cc00d71f9c9f3c66d0c9d5bcfb2ffd
Jo Beattie <beattiejo1@gmail.com>
2015-12-18 15:50:08 +0000

    show button if admin

--- navbar.php
+++ navbar.php
@@ -21,7 +21,7 @@ if(isset($_SESSION['loggedIn'])) {//if user is loggedIn to WanderBlog
     $authorise = '';
     if($row['isAdmin'] == true){
         $authorise = '
-            <a type="submit" class="btn btn-info btn-block" href="' . $profilePath . '">
+            <a class="btn btn-info btn-block" href="#" data-toggle="modal" data-target="#modal-auth">
                 <span class="glyphicon glyphicon-tasks"></span> AUTHORISE
             </a>';
     }
@@ -112,6 +112,49 @@ if(isset($_SESSION['loggedIn'])) {//if user is loggedIn to WanderBlog
 
 				</div>
 			</div>
+<!-- NEW -->
+            <div class="modal fade" id="modal-auth" role="dialog" style="padding-top: 35px;">
+		        <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            <h4><span class="glyphicon glyphicon-tasks"></span> Authorise </h4>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col-sm-3"><p>Full Name: </p></div>
+                                <div class="col-sm-9">\' . $_SESSION["FullName"] . \'</div>
+                             </div>
+                             <div class="row">
+                                <div class="col-sm-3"><p>Username: </p></div>
+                                <div class="col-sm-9">\' . $_SESSION["username"] . \'</div>
+                             </div>
+                            <div class="row">
+                                <div class="col-sm-3"><p>Email Address: </p></div>
+                                <div class="col-sm-9">\' . $row["emailAddress"] . \'</div>
+                             </div>
+                             <div class="row">
+                                <div class="col-sm-3"><p>Password: </p></div>
+                                <div class="col-sm-9">\' . $row["password"] . \'</div>
+                             </div>
+
+                        </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-success btn-block" data-dismiss="modal">
+                                <span class="glyphicon glyphicon-ok"></span> OK
+                            </button>
+                            ' . $myProfile . $authorise . '
+                            <a type="submit" class="btn btn-danger btn-block" onCLick="confirmDelete()">
+                                <!-- <a href="createUser.php?operation=delete"> -->
+                                    <span class="glyphicon glyphicon-trash"></span> DELETE
+                                </a>
+                            </button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
 		</div>
 	</div>
 
