c1669143b99899402544e8197dc39e0ff6ac69d6
Jo Beattie <beattiejo1@gmail.com>
2015-12-18 17:32:35 +0000

    updated query

--- addUser.php
+++ addUser.php
@@ -19,11 +19,12 @@ else if($task == 'OK'){
     //GET ALL DATA AND ADD TO User
     while($row = mysqli_fetch_array($results)){
         echo $username;
-        $queryTwo = "INSERT INTO User VALUES('" . $username . "', '" . $row['password'] . "', '" . $row['firstName'] . "', '" . $row['lastName'] . "', '" . $row['emailAddress'] . ", 0');";
+        $queryTwo = "INSERT INTO User VALUES('" . $username . "', '" . $row['password'] . "', '" . $row['firstName'] . "', '" . $row['lastName'] . "', '" . $row['emailAddress'] . "', 0');";
        echo "query2: " . $queryTwo;
         $queryFour = "DELETE FROM Authorise WHERE userID= '" . $username . "';";
         $resultsTwo = mysqli_query($conn, $queryTwo);
         $resultsFour =  mysqli_query($conn, $queryFour);
         mysqli_close($conn);
+        header("Location: index.php?error=added&user=" . $username);
     }
 }
