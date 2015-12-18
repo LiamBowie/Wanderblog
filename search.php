d86b3e0f7785f80ec3350ad713d9b2075fdbef9a
Jo Beattie <beattiejo1@gmail.com>
2015-12-18 01:43:08 +0000

    included search area in search

--- search.php
+++ search.php
@@ -43,7 +43,7 @@
         }
     </style>
 </head>
-<body style="padding-top: 75px;">
+<body style="padding-top: 0">
 
 <?php include 'navbar.php'; ?>
 
@@ -67,22 +67,8 @@
 ?>
 
 <div class="jumbotron text-center">
-    <h1>Wanderblog</h1>
-    <p>Blogging site specifically for travelers</p>
+    <p>Search</p>
     <?php include'searchArea.php' ?>
-    <!-- SEARCH --><!--
-		<form id="search form" class="form-inline" action="search.php?searchx=advID" method="post">
-			<input type="search" id="crit" name="crit" class="form-control" size="50" placeholder="What are you waiting for?">
-			<select class="form-control" name="select" id="select">
-				<option id="opt-Adventure" value="advID">Adventure (by Title)</option>
-				<option id ="opt-AdvAuthor" value="author">Adventure (by Author)</option>
-				<option id ="opt-Author" value="firstName">Author (by Name)</option>
-			</select>
-			<button type="submit" class="btn btn-danger">
-				<span class="glyphicon glyphicon-search"></span>Search
-			</button>
-		</form>
-<!-- ENF OF SEARCH -->
 </div>
 
     <div class="container-fluid text-center">
