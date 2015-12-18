5c49070f31bdbf66416af73b13804f6fa99e0dcc
Jo Beattie <beattiejo1@gmail.com>
2015-12-18 19:54:08 +0000

    executed query first

--- addUser.php
+++ addUser.php
@@ -20,6 +20,7 @@ else if($task == 'OK'){
     while($row = mysqli_fetch_array($results)){
         echo $username;
         $queryTwo = "INSERT INTO User VALUES('" . $username . "', '" . $row['password'] . "', '" . $row['firstName'] . "', '" . $row['lastName'] . "', '" . $row['emailAddress'] . "', 0);";
+        $resultsTwo = mysqli_query($conn, $queryTwo);
         if ($row['author'] == 1) {
             $found = false;
             $query = 'SELECT * FROM Author';
