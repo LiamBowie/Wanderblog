5dfe21880c9ac02788b17f57cc2d2ae80337d0ce
LiamBowie <liambowie16+GitHub@gmail.com>
2015-12-18 20:27:57 +0000

    Think this should get me back to a working state

--- index.php
+++ index.php
@@ -132,7 +132,7 @@
 		<div class="carousel-inner" roles="listbox">
 			<?php
 			$sqlTop5 = "
-			SELECT Adventure.advID, Adventure.title, CONCAT(User.firstName, ' ', User.lastName) AS authorName, Adventure.author Adventure.photo, Adventure.numVotes
+			SELECT Adventure.title, CONCAT(User.firstName, ' ', User.lastName) AS authorName, Adventure.photo, Adventure.numVotes
 			FROM Adventure
 			LEFT JOIN Author
         	ON Adventure.author=Author.authorID
@@ -156,9 +156,9 @@
 					}
 					echo'	<img src=' . $rowTop5['photo'] . ' alt="Image">
 							<div class="carousel-caption">
-								<h3><a href="adventure.php?adv=' . $rowTop5['advID'] . '" >' . $rowTop5['title'] . '</a></h3>
-								<p><a href="author.php?auth=' . $rowTop5['author'] . '" >' . $rowTop5['authorName'] . '</a></p>
-								<span class="badge"> ' . $rowTop5['numVotes'] . ' votes </span>
+								<h3>' . $rowTop5['title'] . '</h3>
+								<span class="badge"> ' . $rowTop5['numVotes'] .' votes </span>
+								<p>' . $rowTop5['authorName'] . '</p>
 							</div>
 						</div>
 						';
