0a1cf2abde06deed9ba52c02d524dd324f41d067
Jo Beattie <beattiejo1@gmail.com>
2015-12-18 13:17:28 +0000

    added output

--- search.php
+++ search.php
@@ -87,7 +87,7 @@
 
     <div class="container-fluid text-center">
         <div class="col-sm-12 text-left" style="padding-left: 25px; padding-right: 25px">
-            <?php echo "<h2 style = 'color:#ffffff;'>Displaying results for " . $_POST['crit'] . "</h2>" ?>
+            <?php echo "<h2 style = 'color:#ffffff;'>Displaying results for " . $_POST['crit'] . " IN " . $colmSearch . "</h2>" ?>
             <div class="container">
                 <div class="row">
                     <!-- LOOP THROUGH AND OUTPUT FOLLOWING PER EACH -->
