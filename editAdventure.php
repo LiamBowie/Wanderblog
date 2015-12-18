9582305ba7b58061cf253b06d305d7e43db6e030
finlay54 <finlayjohnston60@gmail.com>
2015-12-18 12:21:13 +0000

    Adding createAdventure.php and editAdventure.php

---
+++ editAdventure.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script type=text/javascript" src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="images/earth.ico">
+    <script type="text/javascript" src="script.js"></script>
+    <style>
+        /* Set height of the grid so .sidenav can be 100% (adjust if needed) */
+        .row.content {height: 1500px}
+
+        /* Set gray background color and 100% height */
+        .sidenav {
+            background-color: #f1f1f1;
+            height: 100%;
+        }
+
+        /* Set black background color, white text and some padding */
+        footer {
+            background-color: #555;
+            color: white;
+            padding: 15px;
+        }
+
+        /* On small screens, set height to 'auto' for sidenav and grid */
+        @media screen and (max-width: 767px) {
+            .sidenav {
+                height: auto;
+                padding: 15px;
+            }
+            .row.content {height: auto;}
+        }
+    </style>
+</head>
+<body style="padding-top: 75px;" data-spy="scroll" data-target=".sticky-sidebar" data-offset="50">
+<?php include'navbar.php'; ?>
+
+
+</body>
+</html>
