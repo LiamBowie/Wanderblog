a714cb61bd87ef5fbe93adb14d610f55bc30fe18
Jo Beattie <beattiejo1@gmail.com>
2015-12-20 12:56:47 +0000

    various updates to remove adventure

--- delAdv.php
+++ delAdv.php
@@ -7,16 +7,9 @@ error_reporting(E_ALL);
 
 $adventure = $_GET['adv'];
 
-$queryDel = "
-                DELETE FROM Comments
-                WHERE advID = '" . $adventure . "';
-                DELETE FROM Votes
-                WHERE advID = '" . $adventure . "';
-                DELETE FROM Adventure
-                WHERE advID = '" . $adventure . "';
-        ";
-
-mysqli_query($conn, $queryDel);
+$queryDel = "DELETE FROM Comments WHERE advID = '" . $adventure . "'; DELETE FROM Votes WHERE advID = '" . $adventure . "'; DELETE FROM Adventure WHERE advID = '" . $adventure . "'; ";
+$resultsDel = mysqli_query($conn, $queryDel);
+//mysqli_query($conn, $queryDel);
 mysqli_close($conn);
 
 echo $queryDel;
