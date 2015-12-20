5ef2706bc2d39e334bb800bff84301cdf387f571
Jo Beattie <beattiejo1@gmail.com>
2015-12-20 03:01:03 +0000

    change

--- adventure.php
+++ adventure.php
@@ -140,7 +140,7 @@ $numVotes = $row['numVotes'];
                     }
 
                     if($adminRow['isAdmin'] == 1 || $_SESSION['username'] == $row['userID'] ){
-                        echo "<p></p><p></p>button class='btn btn-danger'>Delete Adventure</pbutton>";
+                        echo "<p></p><p></p>button class='btn btn-danger'>Delete Adventure</button>";
                     }
                     ?>
                 </form>
